60554a504c4ef545cc6ab29d04546cac65b97634
Protomanzero0 <protomanzero01@gmail.com>
2022-01-14 21:18:07 -0700

    Finished TicTacToe game, no bugs yet

--- prototyping.py
+++ prototyping.py
@@ -18,16 +18,53 @@ def playerSwitch(symbol):
     else:
         return "O"
 
+def winCondition(board):
+    if (
+    board[0] == board[1] == board[2] or
+    board[3] == board[4] == board[5] or
+    board[6] == board[7] == board[8] or
+    board[0] == board[3] == board[6] or 
+    board[1] == board[4] == board[7] or
+    board[2] == board[5] == board[8] or
+    board[0] == board[4] == board[8] or 
+    board[2] == board[4] == board[6]):
+        return True
+    else:
+        return False
+
+def drawCondition(turns):
+    if turns >= 9:
+        return True
+    else:
+        return False
+
 def main():
     board = makeBoard()
     displayBoard(board)
-    end = False
+    endGame = False
+    turns = 0
     symbol = ""
-    while end == False:
-        player = playerSwitch(symbol)
-        choice = int(input("X's turn to choose a square (1-9): "))
-        board[choice - 1] = symbol
-        symbol = "O"
-        choice = input(f"{symbol}'s turn to choose a square (1-9): ")
+    player = playerSwitch(symbol)
+    endGameDraw = False
+    while endGame == False and endGameDraw == False:
+        print()
+        choice = int(input(f"{player}'s turn to choose a square (1-9): "))
+        print()
+        if board[choice - 1] != "X" and board[choice - 1] != "O": 
+            board[choice - 1] = player
+            displayBoard(board)
+            turns += 1
+            endGame = winCondition(board)
+            endGameDraw = drawCondition(turns)
+            if endGame == False:
+                player = playerSwitch(player)
+        else:
+            print("That square is taken! Please try again.")
+    if endGame == True:
+        print(f"{player} wins! Good game!")
+    elif endGameDraw == True:
+        print("It's a draw! Good game!")
+    else:
+        print("Lol wut")
 
 main()
